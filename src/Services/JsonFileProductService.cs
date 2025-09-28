47095532732b249b8c51439d466264c12010ac74
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-09-27 23:12:26 -0700

    Add project files.

---
+++ src/Services/JsonFileProductService.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+using ContosoCrafts.WebSite.Models;
+
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.WebSite.Services
+{
+   public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+
+
+        public IEnumerable<ProductModel> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<ProductModel[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public bool AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            if(products.First(x => x.Id == productId).Ratings == null)
+            {
+                products.First(x => x.Id == productId).Ratings = new int[] { rating };
+            }
+            else
+            {
+                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+                ratings.Add(rating);
+                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+            }
+
+            using(var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<ProductModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }), 
+                    products
+                );
+            }
+
+            return true;
+        }
+
+        public bool UpdateData(ProductModel data)
+        {
+            bool isValidUpdate = false;
+            var products = GetProducts();
+            var productData = products.FirstOrDefault(x => x.Id.Equals(data.Id));
+            if (productData == null)
+            {
+                isValidUpdate = false;
+                return isValidUpdate;
+            }
+
+             //var newProductList; 
+            // TODO - You will have to figure out how to get the updated
+            // data into newProductList
+
+            //SaveData(newProductList);
+
+            isValidUpdate = true;
+            return isValidUpdate;
+        }
+
+
+        private void SaveData(IEnumerable<ProductModel> products)
+        {
+
+            using (var outputStream = File.Create(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<ProductModel>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+    }
+}
