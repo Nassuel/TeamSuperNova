4ffff59884a98afccf38e64a9cc39ce8d2da7f07
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-10-16 09:23:10 -0700

    Created new model for SubCategory in SubCategoryModel.cs

---
+++ src/Models/SubCategoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    public class SubCategoryModel
+    {
+        public string Id { get; set; }
+        public string Brand { get; set; }
+        public string ProductName { get; set; }
+        public string url { get; set; }
+        public string ProductDescription { get; set; }
+    }
+}
