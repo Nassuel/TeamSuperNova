c42ef40c9f399cad707ab44e82663668a43c5a60
AartiDashore <aartidashore1@gmail.com>
2025-10-24 23:49:34 -0700

    Added Category, SubCategory and subcategory image path to data\products.json

--- src/Models/SubCategoryModel.cs
+++ src/Models/SubCategoryModel.cs
@@ -7,5 +7,12 @@
         public string ProductName { get; set; }
         public string url { get; set; }
         public string ProductDescription { get; set; }
+
+        /// <summary>
+        /// Adding these 2 for subcategories
+        /// </summary>
+        public string Category { get; set; }      // e.g. "Laptop"
+        public string SubCategory { get; set; }   // e.g. "Dell", "HP"
+        public string Image { get; set; }
     }
 }
