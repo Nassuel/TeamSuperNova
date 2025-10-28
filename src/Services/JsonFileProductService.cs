c1c7cd668fa04ad86814fc5a18969402cb42cb6c
valeran1@seattleu.edu <valeran1@seattleu.edu>
2025-10-27 18:05:29 -0700

    Changed AddRating function so it returns false when Id is nonexistent in JsonFileProductService.cs

--- src/Services/JsonFileProductService.cs
+++ src/Services/JsonFileProductService.cs
@@ -43,15 +43,22 @@ namespace ContosoCrafts.WebSite.Services
         {
             var products = GetProducts();
 
-            if(products.First(x => x.Id == productId).Ratings == null)
+            var product = products.FirstOrDefault(x => x.Id == productId);
+
+            if (product == null)
             {
-                products.First(x => x.Id == productId).Ratings = new int[] { rating };
+                return false;
+            }
+
+            if (product.Ratings == null)
+            {
+                product.Ratings = new int[] { rating };
             }
             else
             {
-                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
+                var ratings = product.Ratings.ToList();
                 ratings.Add(rating);
-                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
+                product.Ratings = ratings.ToArray();
             }
 
             using(var outputStream = File.OpenWrite(JsonFileName))
