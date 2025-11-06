1e48d4a2420a8ea9527fa7c1ce63755881f1d512
AartiDashore <aartidashore1@gmail.com>
2025-11-05 22:36:49 -0800

    Modified OnGet() in Pages\Product\Create.cshtml.cs

--- src/Pages/Product/Create.cshtml.cs
+++ src/Pages/Product/Create.cshtml.cs
@@ -1,12 +1,35 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        public JsonFileProductService ProductService { get; set; }
+        public CreateModel(JsonFileProductService productService)
         {
+            ProductService = productService;
         }
+        public IActionResult OnGet()
+        {
+            var newProduct = new ProductModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "New Product",
+                Description = "Default description",
+                Url = "Enter the Url",
+                Image = "Upload Image",
+            };
+
+            ProductService.AddCategory(newProduct);
+
+            return RedirectToPage("/Product/Update", new { id = newProduct.Id });
+        }
+
     }
 }
+
+
