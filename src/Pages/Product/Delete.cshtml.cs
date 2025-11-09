30a13e5eeae95243ca593038fe6704b784b36fef
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-09 14:54:40 -0800

    Adding "Delete" logic in Delete.cshtml.cs

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -1,12 +1,46 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly JsonFileProductService ProductService;
+
+        public DeleteModel(JsonFileProductService productService)
         {
+            ProductService = productService;
+        }
+
+        [BindProperty]
+        public ProductModel Product { get; set; }
+        public IActionResult OnGet(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToPage("/Product/Index");
+            }
+
+            Product = ProductService.GetProducts().First(p => p.Id == id);
+            if (Product == null)
+            {
+                return RedirectToPage("/Product/Index");
+            }
+
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (Product?.Id == null)
+            {
+                return RedirectToPage("/Product/Index");
+            }
+
+            ProductService.DeleteCategory(Product.Id);
+            return RedirectToPage("/Product/Index");
         }
     }
 }
