1c106e750bf7aab80d7ba74ea655a6c15f676dfb
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-08 22:03:50 -0800

    Adding DisplayName Extension to ProductTypeEnum in ProductType.cs

--- src/Models/ProductType.cs
+++ src/Models/ProductType.cs
@@ -1,4 +1,6 @@
-﻿namespace ContosoCrafts.WebSite.Models
+﻿using System.Net.NetworkInformation;
+
+namespace ContosoCrafts.WebSite.Models
 {
     public enum ProductTypeEnum
     {
@@ -9,5 +11,25 @@
         Headset = 15,
         VrHeadsets = 17,
         Printer3D = 20,
+    }
+
+    public static class ProductTypeEnumExtensions
+    {
+        public static string DisplayName(this ProductTypeEnum data)
+        {
+            return data switch
+            {
+                ProductTypeEnum.Undefined => "Undefined",
+                ProductTypeEnum.Laptop => "Laptop",
+                ProductTypeEnum.Keyboard => "Keyboard",
+                ProductTypeEnum.Mice => "Mice",
+                ProductTypeEnum.Headset => "Headset",
+                ProductTypeEnum.VrHeadsets => "VR Headsets",
+                ProductTypeEnum.Printer3D => "Printer3D",
+
+                // Default, Unknown
+                _ => "",
+            };
+        }
     }
 }
