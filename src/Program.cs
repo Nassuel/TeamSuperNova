139b284981d770d4b323c6c99b843f2fa8bb5bc2
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-13 22:59:02 -0800

    Refactoring to meet coding standards Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,18 +3,47 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+
+    /// <summary>
+    /// Entry point class for the application
+    /// </summary>
     public class Program
     {
+
+        /// <summary>
+        /// Main entry point method that builds and runs the web host
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         public static void Main(string[] args)
         {
+
+            // Create and run the web host
             CreateHostBuilder(args).Build().Run();
+
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        /// <summary>
+        /// Creates and configures the host builder for the web application
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        /// <returns>Configured host builder instance</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+
+            // Create default host builder
+            var hostBuilder = Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+
+                    // Configure web host to use Startup class
                     webBuilder.UseStartup<Startup>();
+
                 });
+
+            return hostBuilder;
+
+        }
+
     }
+
 }
