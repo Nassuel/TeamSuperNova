3355f1b25e33382a35b3c324bd4e86fac8d93659
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-13 22:50:55 -0800

    Refactoring to meet coding standards About.cshtml.cs

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
+    /// <summary>
+    /// Page model for the About page
+    /// </summary>
     public class AboutModel : PageModel
     {
+
+        /// <summary>
+        /// Handles GET request to display the About page
+        /// </summary>
         public void OnGet()
         {
+
         }
+
     }
+
 }
