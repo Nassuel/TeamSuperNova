3b127b4773eb6a395de76545d5a9710f4c393c36
AartiDashore <aartidashore1@gmail.com>
2025-11-13 23:18:10 -0800

    Added test cases for UnitTests\Pages\Privacy.cshtml.Tests.cs

---
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -0,0 +1,117 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace UnitTests.Pages
+//{
+//    internal class Privacy
+//    {
+//    }
+//}
+
+
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit tests for PrivacyModel class
+    /// Tests all methods and properties to achieve 100% code coverage
+    /// </summary>
+    [TestFixture]
+    public class PrivacyModelTests
+    {
+        #region TestSetup
+
+        public static PrivacyModel pageModel;
+        public static Mock<ILogger<PrivacyModel>> mockLogger;
+
+        /// <summary>
+        /// Initialize test environment before each test
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            mockLogger = new Mock<ILogger<PrivacyModel>>();
+            pageModel = new PrivacyModel(mockLogger.Object);
+        }
+
+        #endregion TestSetup
+
+        #region Constructor
+
+        /// <summary>
+        /// Test constructor creates valid instance with logger
+        /// </summary>
+        [Test]
+        public void Constructor_Valid_Should_Create_Valid_Instance()
+        {
+            // Arrange - Done in TestInitialize
+
+            // Act - PageModel created in TestInitialize
+
+            // Assert
+            Assert.That(pageModel, Is.Not.Null);
+        }
+
+        /// <summary>
+        /// Test constructor initializes with logger
+        /// </summary>
+        [Test]
+        public void Constructor_Valid_Should_Initialize_With_Logger()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<PrivacyModel>>();
+
+            // Act
+            var model = new PrivacyModel(logger.Object);
+
+            // Assert
+            Assert.That(model, Is.Not.Null);
+        }
+
+        #endregion Constructor
+
+        #region OnGet
+
+        /// <summary>
+        /// Test OnGet executes without error
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Execute_Without_Error()
+        {
+            // Arrange - Done in TestInitialize
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.That(pageModel.ModelState.IsValid, Is.True);
+        }
+
+        /// <summary>
+        /// Test OnGet can be called multiple times
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Multiple_Calls_Should_Execute_Without_Error()
+        {
+            // Arrange - Done in TestInitialize
+
+            // Act
+            pageModel.OnGet();
+            pageModel.OnGet();
+            pageModel.OnGet();
+
+            // Assert
+            Assert.That(pageModel.ModelState.IsValid, Is.True);
+        }
+
+        #endregion OnGet
+    }
+}
