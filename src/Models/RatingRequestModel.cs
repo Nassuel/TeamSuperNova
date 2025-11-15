dea6986b2ad07ebb7a0dd9937e680488a508cb1a
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-15 11:36:43 -0800

    Creating RatingRequestModel.cs

---
+++ src/Models/RatingRequestModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Model for rating request containing product ID and rating value
+    /// </summary>
+    public class RatingRequest
+    {
+
+        // Unique identifier for the product
+        public string ProductId { get; set; }
+
+        // Rating value for the product
+        public int Rating { get; set; }
+
+    }
+
+}
