25a01cf36e1d666944a9e580996e73a511024cb4
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-15 11:37:00 -0800

    Using RatingRequestModel in ProductsController.cs

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -80,20 +80,6 @@ namespace ContosoCrafts.WebSite.Controllers
 
         }
 
-        /// <summary>
-        /// Model for rating request containing product ID and rating value
-        /// </summary>
-        public class RatingRequest
-        {
-
-            // Unique identifier for the product
-            public string ProductId { get; set; }
-
-            // Rating value for the product
-            public int Rating { get; set; }
-
-        }
-
     }
 
 }
