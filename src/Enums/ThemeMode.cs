652d68423d3faa5a59a23f528b70c14376e75b0f
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-11-20 20:35:37 -0800

    Creating ThemeMode.cs enum to move it to its own folder /Enums

---
+++ src/Enums/ThemeMode.cs
@@ -0,0 +1,17 @@
+﻿namespace ContosoCrafts.WebSite.Enums
+{
+    /// <summary>
+    /// Represents the available theme modes for the application
+    /// </summary>
+    public enum ThemeMode
+    {
+        // Undefined theme mode
+        Undefined = 0,
+
+        // Light theme mode
+        Light = 7,
+
+        // Dark theme mode
+        Dark = 10
+    }
+}
